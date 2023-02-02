b16a566959adf4ab4ec1c87438264f028257ae60
ObscureAngel <Obscure-Angel@gmx.fr>
2023-02-02 23:47:44 +0100

    Update cla_Operation.class.php
    Ajout du constructeur et des méthodes de l'interface

--- model/cla_Operation.class.php
+++ model/cla_Operation.class.php
@@ -6,4 +6,59 @@ use MaBanque\DB\DBInsertion;
 
 class Operation implements DBInsertion {
 	
+
+	/**
+	 */
+	public function __construct() {
+	}
+	/**
+	 * Ajoute un champ dans le table d'insertion
+	 *
+	 * @param array $pa_champsInsertion
+	 * @param string $ps_nomChamp
+	 * @param mixed $pv_valeurChamp
+	 * @return void
+	 */
+	public function fct_ajouteChampInsertion(array &$pa_champsInsertion, string $ps_nomChamp, mixed $pv_valeurChamp) {
+	}
+	
+	/**
+	 * Retire un champ du tableau d'insertion
+	 *
+	 * @param array $pa_champsInsertion
+	 * @param string $ps_nomChamp
+	 * @return void
+	 */
+	public function fct_retireChampInsertion(array &$pa_champsInsertion, string $ps_nomChamp) {
+	}
+	
+	/**
+	 * Modifie un champ du tableau d'insertion
+	 *
+	 * @param array $pa_champsInsertion
+	 * @param string $ps_nomChamp
+	 * @param mixed $pv_valeurChamp
+	 * @return void
+	 */
+	public function fct_modifieChampInsertion(array &$pa_champsInsertion, string $ps_nomChamp, mixed $pv_valeurChamp) {
+	}
+	
+	/**
+	 * Prépare et contrôle les données avant insertion
+	 *
+	 * @param array $pa_champsInsertion
+	 * @return void
+	 */
+	public function fct_prepareInsertion(array &$pa_champsInsertion) {
+	}
+	
+	/**
+	 * Lance l'insertion des données dans la base
+	 *
+	 * @param array $pa_champsInsertion
+	 * @param \cla_PDOMySQL $po_connexionBdd
+	 * @return void
+	 */
+	public function fct_lancementInsertion(array &$pa_champsInsertion, \cla_PDOMySQL $po_connexionBdd) {
+	}
 }
