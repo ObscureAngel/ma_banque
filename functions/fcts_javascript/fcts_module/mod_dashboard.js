8cf913acba6c441f9f0a416a0c1b4cac4c4aebe5
ObscureAngel <Obscure-Angel@gmx.fr>
2023-05-28 19:45:18 +0200

    Update mod_dashboard.js
    Ajout d'un bouton pour envoyer le relevé

--- functions/fcts_javascript/fcts_module/mod_dashboard.js
+++ functions/fcts_javascript/fcts_module/mod_dashboard.js
@@ -22,3 +22,21 @@ document.querySelector("#openBtn").addEventListener('click', function() {
 document.querySelector("#closeBtn").addEventListener('click', function() {
 	document.querySelector("#mySidenav").classList.remove("active");
 });
+
+/*document.querySelector('#btn_envoiReleve').addEventListener('click', function(po_event) {
+	po_event.preventDefault();
+	
+	let fo_formulaireReleve = new FormData();
+	fo_formulaireReleve.append("ur_fichierReleve", document.querySelector('#ur_fichierReleve').files[0]);
+	fo_formulaireReleve.append("ui_idCompte", document.querySelector('#ui_idCompte').value);
+
+	Ajax.fct_ajaxSender(
+		"mb_operation.ajax.php",
+		fo_formulaireReleve,
+		function(po_retourAjax) {
+			console.log(po_retourAjax.ajs_messageRetour);
+
+			if (po_retourAjax.ajb_rechargePage) window.location.reload();
+		}
+	);
+});*/
